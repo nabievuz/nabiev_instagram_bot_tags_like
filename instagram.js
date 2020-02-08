0110e7055c50baf70a03f5ac39fd00a03670665e
Akmaljon Nabiev <36360436+nabievuz@users.noreply.github.com>
2020-02-08 17:05:11 +0500

    New update instagram class name

--- instagram.js
+++ instagram.js
@@ -13,9 +13,9 @@ const instagram = {
 
         instagram.browser = await puppeteer.launch({
             headless: false,
+            defaultViewport: null,
             args: ['--lang=en-US']
         });
-
         instagram.page = await instagram.browser.newPage();
 
     },
@@ -36,7 +36,7 @@ const instagram = {
             waitUntil: "networkidle2"
         });
 
-        await instagram.page.waitFor(4000);
+        await instagram.page.waitFor(2000);
 
         await instagram.page.type('input[name="username"]', username, {
             delay: 50
@@ -48,9 +48,6 @@ const instagram = {
         loginButton = await instagram.page.$x('//div[contains(text(),"Log In")]');
         await loginButton[0].click();
 
-        // await instagram.page.waitFor(10000);
-        // await instagram.page.waitFor('a > span[aria-label="Profile"]');
-
         await instagram.page.waitFor(4000);
         notificationsButton = await instagram.page.$x('//button[contains(text(),"Not Now")]');
         await notificationsButton[0].click();
@@ -58,53 +55,49 @@ const instagram = {
     },
 
     likeTagsProcess: async(tags = []) => {
+        for (let tag of tags) {
 
-        for (let i = 0; i < 3; i++) {
-            for (let tag of tags) {
+            await instagram.page.goto(TAG_URL(tag), {
+                waitUntil: 'networkidle2'
+            });
+			
+            await instagram.page.waitFor(1000);
+			
+            let posts = await instagram.page.$$('article > div:nth-child(3) img[decoding="auto"]');
 
-                await instagram.page.goto(TAG_URL(tag), {
-                    waitUntil: 'networkidle2'
-                });
-                await instagram.page.waitFor(1000);
+            for (let i = 0; i < 6; i++) {
+                let post = posts[i];
 
-                let posts = await instagram.page.$$('article > div:nth-child(3) img[decoding="auto"]');
+                await post.click();
 
-                for (let i = 0; i < 3; i++) {
-                    let post = posts[i];
+                await instagram.page.waitFor('body[style="overflow: hidden;"]');
 
-                    await post.click();
+                let test = await instagram.page.$('body[style="overflow: hidden;"]');
+                console.log(test);
 
-                    await instagram.page.waitFor('body[style="overflow: hidden;"]');
+                await instagram.page.waitFor(2000);
 
-                    let test = await instagram.page.$('body[style="overflow: hidden;"]');
-                    console.log(test);
+                let isLikable = await instagram.page.$('svg[aria-label="Like"]');
 
-                    await instagram.page.waitFor(1000);
+                console.log('isLikable outside if', isLikable);
 
-                    let isLikable = await instagram.page.$('span[aria-label="Like"]');
+                if (isLikable) {
 
-                    console.log('isLikable outside if', isLikable);
+                    console.log('isLikable inside if', isLikable);
+                    await instagram.page.click('svg[aria-label="Like"]');
 
-                    if (isLikable) {
-
-                        console.log('isLikable inside if', isLikable);
-                        await instagram.page.click('span[aria-label="Like"]');
-
-                    }
-
-                    await instagram.page.waitFor(3000);
-
-                    let closeModalButton = await instagram.page.$x('//button[contains(text(), "Close")]');
-
-                    await closeModalButton[0].click();
-
-                    await instagram.page.waitFor(1000);
                 }
 
-                await instagram.page.waitFor(5000);
+                await instagram.page.waitFor(1000);
+
+                let closeModalButton = await instagram.page.$$('button > svg[aria-label="Close"]');
+                await closeModalButton[0].click();
+
+                await instagram.page.waitFor(1000);
             }
+            await instagram.page.waitFor(5000);
         }
     }
-}
+};
 
 module.exports = instagram;
